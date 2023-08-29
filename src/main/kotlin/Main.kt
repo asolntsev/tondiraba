2d6d67580d4530990aec71957ceddd0a2c7c3edd
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-08-29 22:16:51 +0300

    initial project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+import org.jsoup.Jsoup
+import java.net.URL
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println("Program arguments: ${args.joinToString()}")
+    
+    // https://tondirabaicehall.ee/veebikalender/
+    // val html = Jsoup.(URL("https://tondirabaicehall.ee/veebikalender/"))
+}
